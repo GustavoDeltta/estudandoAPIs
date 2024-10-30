ec46e302507f7046dca9e5032fa37ada53906068
GustavoDeltta <delttadois@gmail.com>
2024-10-30 09:04:49 -0300

    Aula 30.10 - Implementada a função de Inserir um novo jogo no front-end.

--- steamApi/steamFrontEnd/script.js
+++ steamApi/steamFrontEnd/script.js
@@ -1,15 +1,42 @@
-async function getAllGames() {
-    var response = await fetch("http://localhost:8080/all");
-    var infos = await response.json();
+var modal = document.querySelector("dialog");
 
-    console.log(infos);
-    for(game of infos){
-        var cardGame = document.createElement("div")
-        cardGame.innerHTML = `
-            <img src="${game.cover}" width="100">
-            <p>${game.name}</p>
-        `;
-        document.querySelector(".allMyGames").appendChild(cardGame)
-    }
+async function getAllGames() {
+  var answer = await fetch("http://localhost:8080/all");
+  var data = await answer.json();
+
+  console.log(data);
+  for (game of data) {
+    var cardGame = document.createElement("div");
+    cardGame.classList.add("cardGame");
+    cardGame.innerHTML = `
+      <img src="${game.cover}" width="120">
+      <p>${game.name}</p>
+    `;
+
+    document.querySelector(".allMyGames").appendChild(cardGame);
+  }
 }
+
+function openModalInsert() {
+  modal.style.display = "flex";
+  modal.showModal();
+}
+
+function closeModalInsert() {
+  modal.style.display = "none";
+  modal.close();
+}
+
+async function insertGame(){
+  var id = Math.floor(Math.random()*1000);
+  var name = document.querySelector("#name");
+  var cover= document.querySelector("#cover");
+
+  var answer = await fetch(`http://localhost:8080/insert?id=${id}&name=${name.value}cover=${cover.value}`);
+  var data = await answer.json();
+
+  modal.style.display = "none";
+  modal.close();
+}
+
 getAllGames();
