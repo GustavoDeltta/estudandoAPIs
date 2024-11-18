fb0c178e33780446832cae08cdea5d5a70553801
GustavoDeltta <deltta.dev@gmail.com>
2024-11-18 10:50:18 -0300

    Aula 18.11 - Criação de endpoints no projeto studentsBD.

--- studentsBd/studentsApi/src/main/kotlin/com/example/studentsApi/studentsController.kt
+++ studentsBd/studentsApi/src/main/kotlin/com/example/studentsApi/studentsController.kt
@@ -2,6 +2,7 @@ package com.example.studentsApi
 
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,5 +12,16 @@ class studentsController(val studentsRepository: studentsRepository) {
     @GetMapping("/all")
     fun getAllStudents(): List<students>{
         return studentsRepository.findAll()
+    }
+    @GetMapping("/insert") // localhost:8080/insert?regi=xxx&name=xxx&age=xxx
+    fun insertStudent(@RequestParam("regi") registration: Int,
+                      @RequestParam("name") name: String,
+                      @RequestParam("age") age: Int): students{
+                            val student = students(registration, name, age)
+                            return studentsRepository.save(student);
+                      }
+    @GetMapping("/search") // localhost:8080/search?age=xx
+    fun getStudentsByAge(@RequestParam("age") age: Int): List<String> {
+        return studentsRepository.getStudentsByAge(age)
     }
 }
--- studentsBd/studentsApi/src/main/kotlin/com/example/studentsApi/studentsRepository.kt
+++ studentsBd/studentsApi/src/main/kotlin/com/example/studentsApi/studentsRepository.kt
@@ -1,7 +1,10 @@
 package com.example.studentsApi
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface studentsRepository: JpaRepository<students, Int> {
-
+    @Query("select name from students where age >= :age", nativeQuery = true)
+    fun getStudentsByAge(@Param("age") age: Int): List<String>
 }
